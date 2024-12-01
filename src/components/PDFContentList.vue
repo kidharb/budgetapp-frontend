f6213f158a6615c2fce217f7f3f34ad6c4cbc2c9
Kidhar Bachan <kidharb@gmail.com>
2024-12-01 21:47:08 +0200

    Alternating coloured rows

--- src/components/PDFContentList.vue
+++ src/components/PDFContentList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>CSV Contents</h1>
-    <table>
+    <table class="fixed-width-table alternate-rows">
       <thead>
         <tr>
           <th>Field2</th>
@@ -61,6 +61,42 @@ export default {
 </script>
 
 <style>
+.fixed-width-table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.fixed-width-table th,
+.fixed-width-table td {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+/* Fixed widths for columns */
+.fixed-width-table th:nth-child(1),
+.fixed-width-table td:nth-child(1) { width: 100px; }
+.fixed-width-table th:nth-child(2),
+.fixed-width-table td:nth-child(2) { width: 150px; }
+.fixed-width-table th:nth-child(3),
+.fixed-width-table td:nth-child(3) { width: 200px; }
+.fixed-width-table th:nth-child(4),
+.fixed-width-table td:nth-child(4) { width: 250px; }
+.fixed-width-table th:nth-child(5),
+.fixed-width-table td:nth-child(5) { width: 300px; }
+.fixed-width-table th:nth-child(6),
+.fixed-width-table td:nth-child(6) { width: 350px; }
+.fixed-width-table th:nth-child(7),
+.fixed-width-table td:nth-child(7) { width: 400px; }
+.fixed-width-table th:nth-child(8),
+.fixed-width-table td:nth-child(8) { width: 450px; }
+.fixed-width-table th:nth-child(9),
+.fixed-width-table td:nth-child(9) { width: 500px; }
+.fixed-width-table th:nth-child(10),
+.fixed-width-table td:nth-child(10) { width: 550px; }
+.fixed-width-table th:nth-child(11),
+.fixed-width-table td:nth-child(11) { width: 600px; }
+
 /* Alternating row colors */
 .alternate-rows tbody tr:nth-child(odd) {
   background-color: #f2f2f2; /* Light grey */
